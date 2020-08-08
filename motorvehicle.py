fe3d065b22ea820b375ea9dba7e546a099a448dc
Georgeskm <69377963+Georgeskm@users.noreply.github.com>
2020-08-08 09:55:36 +0300

    Update motorvehicle.py

--- motorvehicle.py
+++ motorvehicle.py
@@ -5,7 +5,7 @@ import pydeck as pdk
 import plotly.express as px
 
 
-DATA_URL = ("C:/Users/Sarlee/Motor_Vehicle_Collisions_-_Crashes.csv")
+DATA_URL = ("/path/to/Motor_Vehicle_Collisions_-_Crashes.csv")
 
 st.title("Motor Vehicle Collisions in New York City")
 st.markdown("This application is a Streamlit dashboard that can be used "
